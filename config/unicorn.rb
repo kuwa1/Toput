086653f84cd196c9808547bc6ac0ea22c3b0051e
kuwa1 <kuromu36@yahoo.co.jp>
2019-12-01 22:38:05 +0900

    circleciの導入

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,1 +1,7 @@
-./docker/rails/unicorn.rb ./config/
+worker_processes 8
+
+pid "/var/run/unicorn.pid"
+listen "/var/tmp/unicorn.sock"
+
+stdout_path "./log/unicorn.stdout.log"
+stderr_path "./log/unicorn.stderr.log"
